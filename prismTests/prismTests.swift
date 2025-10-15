9296e33f6cbba2f786d49ffa6f523a7ca38adef7
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2025-10-15 12:34:10 -0600

    Initial Commit

---
+++ prismTests/prismTests.swift
@@ -0,0 +1,16 @@
+//
+//  prismTests.swift
+//  prismTests
+//
+//  Created by Andrew Bierman on 10/15/25.
+//
+
+import Testing
+
+struct prismTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
